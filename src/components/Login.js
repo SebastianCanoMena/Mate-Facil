d9122de3d9e7d3478ef2a4f21ab32eee1ea46a1a
SebastianCanoMena <sebastiancanomena@gmail.com>
2023-07-04 12:23:57 -0600

    finished login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,8 +3,22 @@ import react from 'react';
 export default function Login(){
 
     return(
-        <div className='Login'>
-                <h1>Log in</h1>
+        <div className='Login-center'>
+            <div className='Login-center2'>
+
+                <form>
+
+                    <h3 className='Login-h3'>Usuario</h3>
+                    <br/>
+                    <input type='text' placeholder='Username' className='login-input' />
+                    <br/><br/>
+                    <h3 className='Login-h3'>Contraseña</h3>
+                    <br/>
+                    <input type='password' placeholder='Password' className='login-input' />
+                    <button className='Login-button'>Iniciar sesión</button>
+                    <button className='Login-button'>Registrarse</button>
+                </form>
+            </div>
         </div>
     )
 }
